da64ed5669335363096d48aee6041036c7d12d91
astritmehmeti <astritmehmeti000@gmail.com>
2023-03-15 20:27:01 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c0230c87-1e97-40c3-94ad-c84916ecc93c';
+export const MODAL_CLOSE_SEC = 2.5;
